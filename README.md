8c2fad5271adc66aa680d415858685e60c253498
O Meachair, Shane <shane.o.meachair@accenture.com>
2019-11-08 21:57:20 +0000

    add ignored files

--- README.md
+++ README.md
@@ -14,10 +14,12 @@ https://github.com/yaringal/DropoutUncertaintyExps/blob/master/bostonHousing/net
 These scripts can be used as a starting point for implementing deep learning models using dropout Byesian
 approximation.
 
+## Get started
+
 The jupyter [notebook](notebooks/run_bbdl_experiment.ipynb) has provides a quick setup for running some experiments on the CIFAR-10 dataset, 
 comparing MC Dropout to a standard CNN model.
 
-![PRCurve](notebooks/class8.jpg?raw=true)
+![PRCurve](notebooks/class8.jpg)
 
 
 [1] https://arxiv.org/abs/1506.02142
---
+++ src/main/main.py
@@ -0,0 +1,62 @@
+import sys
+import pandas as pd
+import numpy as np
+
+sys.path.append('..')
+sys.path.append('.')
+
+from src.controller.train_model import train_model
+from src.utils.data_utils import accuracy
+from src.utils.metrics import calulate_average_precisions
+
+
+def run_single_experiment(data_samples=1000,
+                   monte_carlo_samples=10,
+                   default_class=1,):
+    """
+
+    Args:
+        data_samples: number of datas amples to use in training
+        monte_carlo_samples: number of MC dropout iterations at test time
+        default_class: (int) return AP scores for this class
+
+    Returns:
+
+    """
+
+    y_test, prediction_df, stochastic_predictions = train_model(n_data_samples=data_samples,
+                                       n_monte_carlo_samples=monte_carlo_samples)
+
+    y_true = pd.Series([int(x)  for x in y_test])
+
+    bayesian_predictions = stochastic_predictions.apply(lambda x: np.argmax(x),
+                                                        axis=1)
+    prediction = prediction_df.apply(lambda x: np.argmax(x), axis = 1)
+
+    confuse_mat_standard = pd.crosstab(y_true, prediction)
+    confuse_mat_bayesian = pd.crosstab(y_true, bayesian_predictions)
+
+    print("Standard confusion matrix: \n {}".format(confuse_mat_standard))
+    print("Bayesian confusion matrix: \n {}".format(confuse_mat_bayesian))
+
+    print("Standard accuracy: {}".format(accuracy(confuse_mat_standard)))
+    print("Bayesian accuracy: {}".format(accuracy(confuse_mat_bayesian)))
+
+
+    standard_ap, stochastic_ap = calulate_average_precisions(default_class,
+                                                             y_true,
+                                                             prediction_df,
+                                                             stochastic_predictions)
+
+    print("Average standard precision on Class {}: {}".format(default_class,
+                                                              standard_ap))
+    print("Average Bayesian precision on Class {}: {}".format(default_class,
+                                                              stochastic_ap))
+
+    return y_true, prediction_df, stochastic_predictions
+
+
+
+if __name__ == "__main__":
+
+    run_single_experiment()
